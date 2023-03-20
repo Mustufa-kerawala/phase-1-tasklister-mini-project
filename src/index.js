b64ce733466c80693ac61ff322739b573a4e1deb
Mustufa-kerawala <mustufakerawala@outlook.com>
2023-03-20 14:46:14 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const priority = document.getElementById("priority");
+  const dueDate = document.getElementById("duration");
+  const taskList = document.getElementById("tasks");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const task = document.getElementById("new-task-description").value;
+    const newTask = document.createElement("li");
+    newTask.innerText = task;
+    taskList.appendChild(newTask);
+    // Adding delete button to the list
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "X";
+    newTask.appendChild(deleteBtn);
+    deleteBtn.addEventListener("click", function(event) {
+      event.preventDefault();
+      newTask.remove();
+      
+    });
+    // Adding priority to the list to be shown next to list item
+    newTask.classList.add(priority.value);
+    // Adding due date to the list to be shown next to list item
+    const dueDate = document.createElement("span");
+    dueDate.innerText = `Due Date: ${document.getElementById("duration").value}`;
+    newTask.appendChild(dueDate);
+    form.reset();
+  }
+  );
+  // console.log(taskList);
 });
+
+
+
+
+
